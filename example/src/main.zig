320f256b681841e71aa4db046e9862a70447f988
Marcus Ramse <marcus@ramse.se>
2024-04-24 22:26:55 +0000

    Update example

--- example/src/main.zig
+++ example/src/main.zig
@@ -8,21 +8,6 @@ var runtime: C.w4on2_rt_t = undefined;
 var player: C.w4on2_player_t = undefined;
 
 fn tone(frequency: u32, duration: u32, volume: u32, flags: u32, _: ?*anyopaque) callconv(.C) void {
-    // w4.tracef(
-    //     "tone %d,%d,%d,%d (%d,%d) (%d,%d,%d,%d) (%d,%d)",
-    //     frequency,
-    //     duration,
-    //     volume,
-    //     flags,
-    //     (frequency >> 0) & 0xffff,
-    //     (frequency >> 16) & 0xffff,
-    //     (duration >> 24) & 0xff,
-    //     (duration >> 16) & 0xff,
-    //     (duration >> 0) & 0xff,
-    //     (duration >> 8) & 0xff,
-    //     (volume >> 0) & 0xff,
-    //     (volume >> 8) & 0xff,
-    // );
     w4.tone(frequency, duration, volume, flags);
 }
 
@@ -32,6 +17,6 @@ export fn start() void {
 }
 
 export fn update() void {
-    C.w4on2_player_tick(&player, &runtime);
+    _ = C.w4on2_player_tick(&player, &runtime);
     C.w4on2_rt_tick(&runtime);
 }
